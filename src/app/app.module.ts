4816d6508bfaa1e69197933e43ba3201ab608a86
Mohamed Samir <MhmdGezo1912@gmail.com>
2021-08-07 13:44:43 +0200

    create new component for states of tasks and style it ,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {SharedModule} from  "./shared/shared.module";
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { TodoTaskComponent } from './components/todo-task/todo-task.component';
+import { TodoStatsComponent } from './components/todo-stats/todo-stats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoFormComponent,
     TodoTaskComponent,
+    TodoStatsComponent,
    
   ],
   imports: [
