0c120fc5ff27b38f6df9661e97cc571032fd5f3c
Mohamed Samir <MhmdGezo1912@gmail.com>
2021-08-07 12:32:10 +0200

    create new component for todo-form , applaying styles to it ,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from  "./shared/shared.module";
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoFormComponent,
+   
   ],
   imports: [
     BrowserModule,
