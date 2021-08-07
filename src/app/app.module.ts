d299a827b09ff0f7c6e55ce3b9dd46a55fb5dbcf
Mohamed Samir <MhmdGezo1912@gmail.com>
2021-08-07 13:05:07 +0200

    create new componet [ todo-task] and finishits styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from  "./shared/shared.module";
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { TodoTaskComponent } from './components/todo-task/todo-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoFormComponent,
+    TodoTaskComponent,
    
   ],
   imports: [
