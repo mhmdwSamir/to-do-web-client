4e771c529c88d4942458fba6359fa66eb3064889
Mohamed Samir <MhmdGezo1912@gmail.com>
2021-08-08 22:36:04 +0200

    fix routing , edit navbar navigation items to real names , add router link property. add navbar routes to routing module ,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { TodoStatsComponent } from './components/todo-stats/todo-stats.component';
+import { TodoTaskComponent } from './components/todo-task/todo-task.component';
 
 const routes: Routes = [
+  { path:  'taskStaste', component: TodoStatsComponent},
+  { path:  'tasks', component: TodoTaskComponent},
   { path:  'signup', component: SignUpComponent}
 
 ];
