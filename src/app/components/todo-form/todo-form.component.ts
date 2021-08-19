7b94af1a469257eef6d23bf6a4bca60020a09ec1
Mohamed Samir <MhmdGezo1912@gmail.com>
2021-08-19 12:10:23 +0200

    handle input element of adding new tasks - add validation on input that user cant add empty task , user cant add task with numbers or special charters . - add some style to error messages

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -6,6 +6,7 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TaskService } from 'src/app/core/services/task.service';
 
 @Component({
@@ -17,11 +18,22 @@ export class TodoFormComponent implements OnInit {
   @ViewChild('inputTask') input!: ElementRef<HTMLInputElement>;
   @Output() onAddTask = new EventEmitter<any>();
 
+  addTaskForm = new FormGroup({
+    task: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(15),
+      Validators.pattern('^[a-zA-Z]+$'),
+    ]),
+  });
   constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.addTaskForm.controls.errors);
+  }
+
   addNewTask(taskContent: string) {
-    if (taskContent) {
+    if (taskContent && this.addTaskForm.valid) {
       this.taskService
         .addTask({ content: taskContent })
         .subscribe((resp: any) => {
